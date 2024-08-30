47b0f60450fc70f057c2aff886e3aa0de8f75571
jaroeichler <88505041+jaroeichler@users.noreply.github.com>
2024-08-30 03:47:36 +0200

    Remove flake-parts

--- flake.nix
+++ flake.nix
@@ -2,110 +2,120 @@
   description = "nixos-config";
 
   inputs = {
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
     git-hooks = {
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
       url = "github:cachix/git-hooks.nix";
     };
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager";
     };
+    nixos-generators = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixlib.follows = "nixpkgs";
+      url = "github:nix-community/nixos-generators";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {
-    flake-parts,
+  outputs = {
     git-hooks,
     home-manager,
+    nixos-generators,
     nixpkgs,
     ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        hooks = git-hooks.lib.${system}.run {
-          hooks = import ./pre-commit-hooks.nix {inherit pkgs;};
-          src = ./.;
-        };
-      in {
-        checks = {
-          pre-commit-check = hooks;
-        };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [];
-          shellHook =
-            hooks.shellHook
-            + '''';
-        };
-      };
-      flake = {
-        nixosConfigurations = {
-          home = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./config-home.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.jaro = import ./home.nix;
-                };
-              }
-            ];
-          };
-          thinkpad = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./config-thinkpad.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.jaro = {
-                    lib,
-                    pkgs,
-                    ...
-                  }: {
-                    imports = [./home.nix];
-                    programs.foot.settings.main.font = lib.mkForce "Hack:size=11";
-                    home.packages = with pkgs; [
-                      acpi
-                    ];
-                  };
-                };
-              }
-            ];
-          };
-          uni = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./config-uni.nix
-              home-manager.nixosModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.jaro = import ./home.nix;
-                };
-              }
-            ];
-          };
-        };
-      };
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    hooks = git-hooks.lib.${system}.run {
+      hooks = import ./pre-commit-hooks.nix {inherit pkgs;};
+      src = ./.;
     };
+
+    config-home = {
+      inherit system;
+      modules = [
+        ./config-home.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.jaro = import ./home.nix;
+          };
+        }
+      ];
+    };
+
+    config-thinkpad = {
+      inherit system;
+      modules = [
+        ./config-thinkpad.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.jaro = {
+              lib,
+              pkgs,
+              ...
+            }: {
+              imports = [./home.nix];
+              programs.foot.settings.main.font = lib.mkForce "Hack:size=11";
+              home.packages = with pkgs; [
+                acpi
+              ];
+            };
+          };
+        }
+      ];
+    };
+
+    config-uni = {
+      inherit system;
+      modules = [
+        ./config-uni.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.jaro = import ./home.nix;
+          };
+        }
+      ];
+    };
+  in {
+    checks.${system} = {
+      pre-commit-check = hooks;
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [];
+      shellHook = hooks.shellHook + '''';
+    };
+
+    packages.${system} = {
+      home = nixos-generators.nixosGenerate (
+        config-home
+        // {format = "iso";}
+      );
+      thinkpad = nixos-generators.nixosGenerate (
+        config-thinkpad
+        // {format = "iso";}
+      );
+      uni = nixos-generators.nixosGenerate (
+        config-uni
+        // {format = "iso";}
+      );
+    };
+
+    nixosConfigurations = {
+      home = nixpkgs.lib.nixosSystem config-home;
+      thinkpad = nixpkgs.lib.nixosSystem config-thinkpad;
+      uni = nixpkgs.lib.nixosSystem config-uni;
+    };
+  };
 }
