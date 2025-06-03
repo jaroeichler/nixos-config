f894f6c0cf4f1dc2f1bdd230caf41562c4a687d8
jaroeichler <88505041+jaroeichler@users.noreply.github.com>
2025-06-03 15:50:57 +0200

    Switch Hyprland keybinds

--- home.nix
+++ home.nix
@@ -192,9 +192,9 @@
       };
       bind = [
         # Basics
-        "Mod1, Backspace, killactive"
-        "Mod1, Return, exec, google-chrome-stable"
-        "Mod1, Delete, exec, ghostty"
+        "Mod1, Delete, killactive"
+        "Mod1, Backspace, exec, google-chrome-stable"
+        "Mod1, Return, exec, ghostty"
         # Focus
         "Mod1, h, movefocus, l"
         "Mod1, j, movefocus, d"
